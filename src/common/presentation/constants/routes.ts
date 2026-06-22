3dd61b7bbc446e2feeea53bc01c0fafb390e2ff4
haroldCoder <haroldc2005@gmail.com>
2026-06-22 18:24:52 -0500

    feat: implement role-based navigation menu and dynamic page titles in the header

---
+++ src/common/presentation/constants/routes.ts
@@ -0,0 +1,33 @@
+export const routes = {
+    home: {
+        path: "/home",
+        label: "Dashboard"
+    },
+    auth: {
+        path: "/",
+    },
+    pets: {
+        path: "/home/pets",
+        label: "Mascotas"
+    },
+    history: {
+        path: "/home/history",
+        label: "Historial de citas"
+    },
+    appointments: {
+        path: "/home/appointments",
+        label: "Próximas citas"
+    },
+    schedule: {
+        path: "/home/schedule",
+        label: "Programar cita"
+    },
+    documents: {
+        path: "/home/documents",
+        label: "Documentos de las mascotas"
+    },
+    vaccinations: {
+        path: "/home/vaccinations",
+        label: "Vacunas"
+    },
+};
